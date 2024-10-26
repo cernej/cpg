f0cd827f25f84b553c57b11249d59b3533c5074d
cernej <cernej@email.cz>
2024-10-26 16:24:32 +0000

    levenstein

--- cviceni2-3.py
+++ cviceni2-3.py
@@ -3,11 +3,24 @@ def levensteinova_vzdalenost(dotaz1, dotaz2):
     Levensteinova vzdalenost říka, jak jsou 2 řetězce rozdílné, pokud jsou stejné je Levensteinova vzdalenost 0,
     pro řetězce "čas" a "čaj" je Levensteinova vzdalenost 1 (liší se v 1 písmenu)
     """
-    pass
+    i = 0
+    lenght = min(len(dotaz1), len(dotaz2))
+    levenstein = 0
+    while i < lenght:
+        if dotaz1[i] != dotaz2[i]:
+            levenstein += 1
+        i += 1
+    levenstein += abs(len(dotaz1) - len(dotaz2))
+    return levenstein
 
 
 def levensteinova_vzdalenost_for(dotaz1, dotaz2):
-    pass
+    levenstein = 0
+    for pismeno1, pismeno2 in zip(dotaz1, dotaz2):
+        if pismeno1 != pismeno2:
+            levenstein += 1
+    levenstein += abs(len(dotaz1) - len(dotaz2))
+    return levenstein
 
 
 if __name__ == "__main__":
@@ -20,5 +33,4 @@ if __name__ == "__main__":
     print(levensteinova_vzdalenost_for(query1, query2))
     print(levensteinova_vzdalenost_for(query2, query3))
     print(levensteinova_vzdalenost_for(query1, query3))
-
     print(levensteinova_vzdalenost_for(query1, query4))
