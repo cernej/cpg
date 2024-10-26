d91f85fc2d70fc14cf02abe20e1794a3616c6f4d
cernej <cernej@email.cz>
2024-10-26 15:57:42 +0000

    upravy

---
+++ cviceni2-2.py
@@ -0,0 +1,20 @@
+def vrat_3_hodnoty():
+    x = 5
+    return x, 6, 1.7
+
+if __name__ == "__main__":
+
+    # seznam = []
+    # while True:
+    #     value = input("Zadej ovoce: ")
+    #     if value == "exit":
+    #         break
+    #     seznam.append(value)
+    # print(seznam)
+
+    #print((1,2,3) + (4,5,6))
+
+    prvni, druha, treti = vrat_3_hodnoty()
+    print(prvni)
+    print(druha)
+    print(treti)
